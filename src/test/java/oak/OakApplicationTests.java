1d76dda64531254dfbce9a15c3af88c8b815ed5e
AlexOak <dubovec995@gmail.com>
2021-10-13 11:18:14 +0200

    create_project

--- src/test/java/oak/OakApplicationTests.java
+++ src/test/java/oak/OakApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package oak;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class OakApplicationTests {
 
 	@Test
 	void contextLoads() {
